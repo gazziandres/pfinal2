c6fb11134201464fdf72de765b98094078a988d2
gazzi <gazzi.andres@gmail.com>
2017-12-07 21:18:35 -0300

    omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,16 +9,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # More info at:
   # https://github.com/plataformatec/devise#omniauth
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
+  # Debemos crear el método .from_omniauth en nuestro modelo User (app/models/user.rb)
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    
+    if @user.persisted?
+      sign_in_and_redirect @user
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      redirect_to root_path, notice: 'Error al iniciar sesión con Facebook.'
+    end
+  end
   # protected
 
   # The path used when OmniAuth fails
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205162138) do
+ActiveRecord::Schema.define(version: 20171208001456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,9 @@ ActiveRecord::Schema.define(version: 20171205162138) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
